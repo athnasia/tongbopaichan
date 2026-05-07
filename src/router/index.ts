e193d1c1e67a871b90baaeb95c7b1d2f085aa489
athanasiahoro <athanasiahoro@gmail.com>
2026-05-07 21:59:25 +0800

    fix: router or base path for white screen

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AppShell from '@/layouts/AppShell.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
